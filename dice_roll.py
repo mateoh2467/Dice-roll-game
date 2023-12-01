73d091fc142f97a371ac1fc7e96ad39bbdd96a17
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-12-01 15:16:08 +0000

    initial commit

---
+++ dice_roll.py
@@ -0,0 +1,18 @@
+# get random number from library
+from random import randint
+computer = randint(1, 6)
+die_1 = randint(1, 6)
+die_2 = randint(1, 6)
+total_die = die_1 + die_2
+# create variables
+
+# welcome and initial input - computer chooses number and asks you for your number 
+person_guess = input("Welcome to the game! To play the dice roll game, you and the computer will choose a number between 1 and 12, and two dice are rolled. Whoever has the number is equal to the sum of the values on the two die wins. Enter a number to begin: ")
+# random number is rolled 
+
+# whoever's number is the same wins 
+if person_guess == total_die
+    input("You win! Play again? y/n: ")
+elif computer_guess == total_die 
+    input("You lost. :( Play again? y/n: ")
+# play again? 
